4d854671c80321bff2a55085ba66db1db73b4496
ibn <officialibn001@gmail.com>
2024-02-28 17:06:11 +0100

    Login screen complete

--- App/Shared/Styles.tsx
+++ App/Shared/Styles.tsx
@@ -6,12 +6,14 @@ import {
 	heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
+// CONTAINER STYLES
 export const containerStyle = StyleSheet.create({
 	container: {
 		paddingHorizontal: wp(3),
 	},
 });
 
+// TEXT STYLES
 export const TextStyles = StyleSheet.create({
 	title: {
 		fontSize: hp(3.5),
@@ -21,5 +23,45 @@ export const TextStyles = StyleSheet.create({
 	textBase: {
 		fontSize: hp(2.2),
 		fontFamily: "Raleway_Light",
+	},
+});
+
+// TEXT INPUT STYLES
+export const TextInputStyles = StyleSheet.create({
+	container: {
+		flexDirection: "row",
+		alignItems: "center",
+		backgroundColor: "#F7F7F9",
+		paddingVertical: hp(2.2),
+		paddingHorizontal: hp(2.5),
+		borderRadius: 30,
+	},
+	input: {
+		// borderWidth: 1,
+		flex: 1,
+		backgroundColor: "transparent",
+		fontSize: hp(2.2),
+		fontFamily: "Raleway_Light",
+	},
+
+	showHideButton: {
+		width: "12%",
+		alignItems: "center",
+		justifyContent: "center",
+		// borderWidth: 1,
+	},
+
+	signInButton: {
+		width: "100%",
+		borderRadius: 20,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#24BAEC",
+		paddingVertical: hp(2),
+	},
+	signInButtonText: {
+		fontSize: hp(2.2),
+		fontFamily: "Raleway_SemiBold",
+		color: "#fff",
 	},
 });
--- Screens/LoginScreen.tsx
+++ Screens/LoginScreen.tsx
@@ -1,9 +1,33 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React, { useEffect } from "react";
+import { FontAwesome, FontAwesome6, Feather } from "@expo/vector-icons";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	SafeAreaView,
+	StyleSheet,
+	TextInput,
+	Touchable,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import {
+	TextInputStyles,
+	TextStyles,
+	containerStyle,
+} from "../App/Shared/Styles";
+
+// RESPONSIVE VALUE LIBRARY
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
 const LoginScreen = () => {
+	// PASSWORD VISIBLE STATE
+	const [showPassword, setShowPassword] = useState(false);
+
 	useEffect(() => {
 		AsyncStorage.setItem("appLaunched", "launched");
 	}, []);
@@ -12,20 +36,163 @@ const LoginScreen = () => {
 		AsyncStorage.removeItem("appLaunched");
 	};
 	return (
-		<View
-			style={{
-				alignItems: "center",
-				justifyContent: "center",
-				flex: 1,
-				gap: 50,
-			}}>
-			<Text>LoginScreen</Text>
+		<SafeAreaView style={styles.container}>
+			{/* CONTAINER */}
+			<View style={containerStyle.container}>
+				{/* TEXT CONTAINER */}
+				<View style={styles.headerTextContainer}>
+					{/* TITLE */}
+					<Text style={TextStyles.title}>Sign in now</Text>
+					{/* SUB TITLE */}
+					<Text style={TextStyles.textBase}>
+						Please sign in to continue our app
+					</Text>
+				</View>
 
-			<TouchableOpacity onPress={() => clear()}>
-				<Text>Clear</Text>
-			</TouchableOpacity>
-		</View>
+				{/* FORM CONTAINER */}
+				<View style={styles.formContainer}>
+					{/* EMAIL CONTAINER */}
+					<View style={TextInputStyles.container}>
+						<TextInput
+							style={TextInputStyles.input}
+							placeholder='example@example.com'
+							placeholderTextColor={"#000"}
+						/>
+					</View>
+
+					{/* PASSWORD CONTAINER */}
+					<View style={TextInputStyles.container}>
+						<TextInput
+							style={TextInputStyles.input}
+							placeholder='Enter Password'
+							secureTextEntry={showPassword}
+							placeholderTextColor={"#000"}
+						/>
+
+						<TouchableOpacity
+							style={TextInputStyles.showHideButton}
+							onPress={() => setShowPassword((prev) => !prev)}>
+							<Feather
+								name={!showPassword ? "eye-off" : "eye"}
+								size={24}
+								color='black'
+							/>
+						</TouchableOpacity>
+					</View>
+
+					{/* FORGET PASSWORD */}
+					<TouchableOpacity
+						activeOpacity={0.5}
+						style={{ width: "auto", marginLeft: "auto" }}>
+						<Text style={styles.forgetPasswordText}>Forgot Password?</Text>
+					</TouchableOpacity>
+
+					{/* SUBMIT BUTTON CONTAINER */}
+					<TouchableOpacity
+						activeOpacity={0.5}
+						style={{ ...TextInputStyles.signInButton, marginTop: hp(4) }}>
+						<Text style={TextInputStyles.signInButtonText}>Sign In</Text>
+					</TouchableOpacity>
+
+					{/* SIGNUP PAGE LINK CONTAINER */}
+					<View style={styles.headerTextContainer}>
+						{/* SUB TITLE */}
+
+						<View style={{ flexDirection: "row", alignItems: "center" }}>
+							<Text style={TextStyles.textBase}>Don't have an account?</Text>
+							<Text
+								style={{
+									...TextStyles.textBase,
+									color: "#FF7029",
+									fontFamily: "Raleway_Medium",
+								}}>
+								{" "}
+								Sign up
+							</Text>
+						</View>
+						{/* SUB TITLE */}
+						<Text style={TextStyles.textBase}>Or connect</Text>
+					</View>
+				</View>
+
+				{/* SOCAIL ICON CONTAINER  */}
+				<View style={styles.socialContainer}>
+					{/* FACEBOOK */}
+					<TouchableOpacity
+						activeOpacity={0.5}
+						style={{ ...styles.socialIcon, backgroundColor: "#F7F7F9" }}>
+						<FontAwesome
+							name='facebook'
+							size={40}
+							color='#1877F2'
+						/>
+					</TouchableOpacity>
+
+					{/* INSTAGRAM */}
+					<TouchableOpacity
+						activeOpacity={0.5}
+						style={{ ...styles.socialIcon, backgroundColor: "#F7F7F9" }}>
+						<FontAwesome
+							name='instagram'
+							size={40}
+							color='#F50000'
+						/>
+					</TouchableOpacity>
+
+					{/* TWITTER */}
+					<TouchableOpacity
+						activeOpacity={0.5}
+						style={{ ...styles.socialIcon, backgroundColor: "#F7F7F9" }}>
+						<FontAwesome6
+							name='x-twitter'
+							size={40}
+							color='#03A9F4'
+						/>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</SafeAreaView>
 	);
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "#fff",
+		flex: 1,
+	},
+	headerTextContainer: {
+		alignItems: "center",
+		gap: hp(1.5),
+		marginTop: hp(4.5),
+	},
+	formContainer: {
+		paddingHorizontal: wp(2),
+		marginTop: hp(5),
+		gap: hp(2),
+	},
+	forgetPasswordText: {
+		fontSize: hp(1.8),
+		fontFamily: "Raleway_Medium",
+		textAlign: "left",
+		color: "#FF7029",
+	},
+	socialContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		gap: wp(5),
+		marginTop: hp(10),
+	},
+	socialIcon: {
+		// padding: hp(2),
+		borderRadius: hp(4),
+		width: hp(7),
+		height: hp(7),
+		// borderWidth: 2,
+		// borderColor: "#707B81",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
